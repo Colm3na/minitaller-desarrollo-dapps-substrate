f82db7570fc46d86fde96e5e75905c3afef5a3b6
mariopino <info@quequiereshacer.es>
2020-11-24 16:48:41 +0100

    example component

---
+++ components/Example.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="example-component">
+    <h1 class="title">{{ title }}</h1>
+    <b-button @click="changeTitle()">Change title</b-button>
+    <p class="mt-4">My number is {{ myNumber }}</p>
+    <b-button @click="increaseNumber()">Increase number</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Old title',
+      myNumber: 0,
+      myArray: [],
+    }
+  },
+  created() {
+    this.myNumber = 5
+  },
+  methods: {
+    changeTitle() {
+      this.title = 'New title'
+    },
+    increaseNumber() {
+      this.myNumber++
+    },
+  },
+}
+</script>
+
+<style>
+.example-component {
+  color: blue;
+  font-weight: 700;
+}
+</style>
---
+++ pages/component.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">Mini taller de desarrollo de dApps sobre Substrate</h1>
+      <Example />
+    </div>
+  </div>
+</template>
+
+<script>
+import Example from '@/components/Example.vue'
+export default {
+  components: {
+    Example,
+  },
+}
+</script>
+
+<style></style>
